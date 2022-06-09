dacc92ffaa199f603e32998e29d29c147daa5a37
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-09 15:37:14 +0300

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -69,5 +69,5 @@ expect(superbowlWin(record)).to.equal('2015');
   it('returns undefined if the record has no win objects', function() {
     const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
     expect(superbowlWin(sadReality)).to.equal(undefined)
-  });
+  }); 
 });
