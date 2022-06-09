3dacdf9569a2afb0e7e01742efa61ad4aab550e7
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-09 15:36:25 +0300

    commit tree fb96a3458acde4e56fdf107d5bb63d101fc684be

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const superbowlWin = (record) => {
+    for (const item of record) {
+        if(item.result === "W"){
+            return item.year
+        }
+    }
+}
+const sadReality = [
+    {result: "N/A"}
+] 
+
+
